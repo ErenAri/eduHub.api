fa483e015154b62ca176201ea8f29174e929fd65
ErenAri <erenari27@gmail.com>
2025-12-14 18:38:23 +0300

    Fix Npgsql setup (remove password provider delegates)

--- src/eduHub.Infrastructure/ServiceCollectionExtensions.cs
+++ src/eduHub.Infrastructure/ServiceCollectionExtensions.cs
@@ -1,23 +1,17 @@
-﻿using Azure.Core;
-using Azure.Identity;
-using eduHub.Application.Interfaces.Buildings;
-using eduHub.Application.Interfaces.Reservations;
+﻿using eduHub.Application.Interfaces.Buildings;
 using eduHub.Application.Interfaces.Rooms;
+using eduHub.Application.Interfaces.Reservations;
 using eduHub.Application.Interfaces.Users;
 using eduHub.Infrastructure.Persistence;
 using eduHub.Infrastructure.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Npgsql;
 
 namespace eduHub.Infrastructure;
 
 public static class ServiceCollectionExtensions
 {
-    private static readonly TokenRequestContext PgTokenScope =
-        new(new[] { "https://ossrdbms-aad.database.windows.net/.default" });
-
     public static IServiceCollection AddInfrastructure(
         this IServiceCollection services,
         IConfiguration configuration)
@@ -26,30 +20,9 @@ public static class ServiceCollectionExtensions
         if (string.IsNullOrWhiteSpace(connectionString))
             throw new InvalidOperationException("ConnectionStrings:DefaultConnection is not configured.");
 
-        var useManagedIdentity = configuration.GetValue("Database:UseManagedIdentity", false);
-
-        services.AddSingleton<NpgsqlDataSource>(_ =>
+        services.AddDbContext<AppDbContext>(options =>
         {
-            if (!useManagedIdentity)
-                return new NpgsqlDataSourceBuilder(connectionString).Build();
-
-            var builder = new NpgsqlDataSourceBuilder(connectionString);
-            var credential = new DefaultAzureCredential();
-
-            builder.UsePasswordProvider(
-                (host, port, database, username) =>
-                    credential.GetToken(PgTokenScope).Token,
-                async (host, port, database, username, cancellationToken) =>
-                    (await credential.GetTokenAsync(PgTokenScope, cancellationToken)).Token
-            );
-
-            return builder.Build();
-        });
-
-        services.AddDbContext<AppDbContext>((sp, options) =>
-        {
-            var dataSource = sp.GetRequiredService<NpgsqlDataSource>();
-            options.UseNpgsql(dataSource);
+            options.UseNpgsql(connectionString);
         });
 
         services.AddScoped<IBuildingService, BuildingService>();
